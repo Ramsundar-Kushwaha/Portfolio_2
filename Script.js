1290c8145745cb7fd81d4db0f984ee0271dba778
Ramsundar Kushwaha <ramsundarkushwaha31@gmail.com>
2025-07-02 07:38:05 +0545

    css for tab

--- Script.js
+++ Script.js
@@ -5,8 +5,6 @@ function scrollToSection(id)
   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
 
-// document.getElementById("contactForm").action = "https://formspree.io/f/xnnvvvbq";
-
 // Initialize EmailJS
 (function () {
   emailjs.init({
